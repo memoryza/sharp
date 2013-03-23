50dfe0719bb7eb063bfe08ba0ac071fd0fc1fcf7
wov <wovfeng@gmail.com>
2013-03-23 21:04:28 +0800

    add or shark

--- ios/www/js/sharp.js
+++ ios/www/js/sharp.js
@@ -22,11 +22,8 @@ function initChess(){
 }
 
 
-
-
 Board.addEventListener(clickEvent,function(e){
  if(turn !== 'human'){return false;}
-
     var _target = e.target;
     var x,y;
     e = e.changedTouches ? e.changedTouches[0] : e;
@@ -40,8 +37,6 @@ Board.addEventListener(clickEvent,function(e){
     x -= Board.offsetLeft;
     y -= Board.offsetTop;
 
-    console.log(x + ',' + y);
-
     var w,h;
     w = Board.clientWidth;
     h = Board.clientHeight;
@@ -50,7 +45,7 @@ Board.addEventListener(clickEvent,function(e){
     var _v2 = ~~(x/w*3);
 
     if(!array[_v1]){return false;}
-    if(array[_v1][_v2] !== null){return false;}
+    if(array[_v1][_v2] !== null){chesses[_v1*3 + _v2].shake(); return false;}
     array[_v1][_v2] = 'you';
     next();
 },false);
@@ -212,14 +207,10 @@ function findPuts(){
 
         }
     }
-
-    // console.log('=============')
-    // console.log(wight_array[0]);
-    // console.log(wight_array[1]);
-    // console.log(wight_array[2]);
-
     return wight_array;
 }
+
+
 
 function render(){
     for(var i=0;i<array.length;i++){
@@ -308,149 +299,6 @@ function judgeWin(){
     }
     return finished;
 }
-
-
-//
-doc.querySelector('.board').addEventListener('touchend',function(){});
-
-
-
-//<----------------- Chess class
-var Chess = Chess || {};
-(function(exports){
-
-    exports.count = 0;
-    exports.first = null;      //the first Chess in the square
-    exports.last = null;   //the last Chess in the square
-    exports.chesses = [];
-    "setX setO setW shake".split(" ").forEach(function(name){
-        exports[name] = function(){
-            exports.chesses.forEach(function(chess){
-                chess[name]();
-            })
-        }
-    })
-
-
-
-    exports.create = function(param){
-        var newChess = Object.create(chessFn);
-        var def = {
-            id:null,
-            elem:null,
-            status:"w",
-            parent:$$(".board").get(0),
-            template:'<div class="chess"><div class="shadow animated"></div><div class="trig animated hinge"><div class="rotate animated"><div class="faceO face"></div><div class="faceX face"></div><div class="faceW face"></div></div></div></div>'
-        };
-        $$.extend(def, param);
-        $$.extend(newChess, def);
-        newChess.init();
-        //record the first and the last one
-        if(exports.count === 0){
-            exports.first = newChess;
-        }
-        else{
-            exports.last = newChess;
-        }
-        exports.chesses.push(newChess);
-        newChess.id = exports.count;
-        exports.count++;
-        return newChess;
-    }
-
-    var chessFn = {
-        init:function(){
-            var that = this;
-            this.elem = $$(this.template).get(0);
-            this.shadowElem = $$(this.elem).find(".shadow").get(0);
-
-            $$(this.elem).on("webkitTransitionEnd", function(){
-                that._transitionend();
-            })
-            //animationend
-            $$(this.elem).on("webkitAnimationEnd", function(){
-                that._animationend();
-            })
-
-            $$(this.parent).append(this.elem);
-        },
-        setStyle:function(){
-            var __elem = $$(this.elem);
-            __elem.style.apply(__elem, arguments);
-        },
-        clearAnim:function(){
-            $$(this.elem)
-            .removeClass("o2w")
-            .removeClass("o2x")
-            .removeClass("x2w")
-            .removeClass("x2o")
-            .removeClass("w2x")
-            .removeClass("w2o");
-        },
-        setX:function(){
-            if(!(this.status === "x")){
-                this.showShadow();
-                this.clearAnim();
-            }
-            if(this.status === "w"){
-                $$(this.elem).addClass("w2x");
-            }
-            else if(this.status === "o"){
-                $$(this.elem).addClass("o2x");
-            }
-            this.status = "x";
-        },
-        setO:function(){
-            if(!(this.status === "o")){
-                this.showShadow();
-                this.clearAnim();
-            }
-            if(this.status === "w"){
-                $$(this.elem).addClass("w2o");
-            }
-            else if(this.status === "x"){
-                $$(this.elem).addClass("x2o");
-            }
-            this.status = "o";
-        },
-        setW:function(){
-            if(!(this.status === "w")){
-                this.showShadow();
-                this.clearAnim();
-            }
-            if(this.status === "x"){
-                $$(this.elem).addClass("x2w");
-            }
-            else if(this.status === "o"){
-                $$(this.elem).addClass("o2w");
-            }
-            this.status = "w";
-        },
-        shake:function(){
-            $$(this.elem).addClass("shake");
-        },
-        _transitionend:function(){
-            if($$(this.elem).hasClass("showShadow")){
-                this.hideShadow();
-            }
-        },
-        _animationend:function(){
-            $$(this.elem).removeClass("shake");
-        },
-        showShadow:function(){
-            $$(this.elem).addClass("showShadow");
-            $$(this.shadowElem).addClass("flash");
-            this.setStyle("-webkit-transform", "translateZ(1000px)");
-        },
-        hideShadow:function(){
-            $$(this.elem).removeClass("showShadow");
-            $$(this.shadowElem).removeClass("flash");
-            this.setStyle("-webkit-transform", "translateZ(0px)");
-        }
-    }
-
-})(Chess)
-//cube class------------------->
 
 
 
